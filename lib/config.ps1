05337045a1dd528226266ea44650a4033a3eedeb
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-04-03 10:27:09 -0400

    Update config.ps1

--- lib/config.ps1
+++ lib/config.ps1
@@ -1,1 +1,14 @@
+# ===== WINFETCH CONFIGURATION =====
 
+# Add a '#' to any of the lines in 
+# this file to enable their output.
+
+# $show_os                  = $false
+# $show_computer            = $false
+# $show_uptime              = $false
+# $show_cpu                 = $false
+# $show_gpu                 = $false
+# $show_memory              = $false
+# $show_disk                = $false
+
+$show_pwsh                = $false
