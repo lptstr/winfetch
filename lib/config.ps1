654faaa81f7e868f1c63823648b1d4e0dd8276cf
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-04-05 19:50:34 -0400

    Update config.ps1

--- lib/config.ps1
+++ lib/config.ps1
@@ -3,18 +3,15 @@
 # Add a '#' to any of the lines in 
 # this file to **enable** their output.
 
-# $show_title               = $true
-# $show_dashes              = $true
-# $show_os                  = $false
-# $show_computer            = $false
-# $show_uptime              = $false
-# $show_terminal            = $false
-# $show_cpu                 = $false
-# $show_gpu                 = $false
-# $show_memory              = $false
-# $show_disk                = $false
-# $show_temperature         = $false
-# $show_pkgs                = $false
-
-$show_pwsh                = $false
-$show_pkgmngr             = $false
+$show_title             = $false
+$show_dashes            = $false
+$show_os                = $false
+$show_computer          = $false
+$show_uptime            = $false
+$show_terminal          = $false
+$show_cpu               = $false
+$show_gpu               = $false
+$show_memory            = $false
+$show_disk              = $false
+$show_pwsh              = $false
+$show_pkgs              = $false
