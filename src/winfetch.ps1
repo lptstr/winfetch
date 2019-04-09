13932f0ae178c1639077222a97e99da90350c967
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-04-09 07:52:02 -0400

    copyright: update

--- src/winfetch.ps1
+++ src/winfetch.ps1
@@ -2,7 +2,7 @@
 #requires -version 5
 
 # The MIT License (MIT)
-# Copyright (c) 2019 Kied Llaentenn
+# Copyright (c) 2019 Kied Llaentenn and contributers
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
