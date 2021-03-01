c0a33a3a4d04a7059f8214e65419093b8407d125
Rashil Gandhi <rashil2000@gmail.com>
2021-03-01 18:14:07 +0530

    Add License and Icon links

--- winfetch.ps1
+++ winfetch.ps1
@@ -1,39 +1,17 @@
 #!/usr/bin/env pwsh
 #requires -version 5
 
-# MIT License
-#
-# Copyright (c) 2021 Kied Llaentenn and contributers
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 <#PSScriptInfo
 .VERSION 2.0.0
 .GUID 1c26142a-da43-4125-9d70-97555cbb1752
 .DESCRIPTION Winfetch is a command-line system information utility for Windows written in PowerShell.
-.AUTHOR lptstr
+.AUTHOR Kied Llaentenn and contributers
 .PROJECTURI https://github.com/lptstr/winfetch
 .COMPANYNAME
 .COPYRIGHT
-.TAGS
-.LICENSEURI
-.ICONURI
+.TAGS neofetch screenfetch system-info commandline
+.LICENSEURI https://github.com/lptstr/winfetch/blob/master/LICENSE
+.ICONURI https://lptstr.github.io/lptstr-images/proj/winfetch/logo.png
 .EXTERNALMODULEDEPENDENCIES
 .REQUIREDSCRIPTS
 .EXTERNALSCRIPTDEPENDENCIES
